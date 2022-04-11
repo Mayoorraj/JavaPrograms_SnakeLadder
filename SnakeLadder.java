8533e4c439e55cf350785694d9ea70b00a2c4746
Mayoor <mayoordoiphode@gmail.com>
2022-04-11 23:51:34 +0530

    player gets no play, snake or ladder

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,5 +7,26 @@ public class SnakeLadder {
 		int playerPos = 0;
 		byte dice = (byte)(Math.random()*6 + 1 );
 		System.out.println("Dice Show: " + dice);
+		byte playerOptions = (byte)(Math.random()*3 );
+		
+		switch (playerOptions) {
+		
+		case 0:
+			System.out.println("Player stays at same postion");
+			break;
+			
+		case 1:
+			System.out.println("Player played & got a Ladder");
+			playerPos = playerPos + dice;
+			break;
+			
+		default:
+			System.out.println("Player played & got a Snake");
+			playerPos = playerPos - dice;
+			if (playerPos <= 0) {
+				playerPos = 0;}
+			break;	
+		}
+		System.out.println("Player is at position : " + playerPos);
 	}
 }
