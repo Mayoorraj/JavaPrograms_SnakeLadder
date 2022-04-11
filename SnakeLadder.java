edb556cf5cc17cd0ca3d00d8283822da431fff3d
Mayoor <mayoordoiphode@gmail.com>
2022-04-11 23:45:45 +0530

    Diceroll

---
+++ SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.assignment4.SnakeLadder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Snake And ladder Program");	
+		int playerPos = 0;
+		byte dice = (byte)(Math.random()*6 + 1 );
+		System.out.println("Dice Show: " + dice);
+	}
+}
