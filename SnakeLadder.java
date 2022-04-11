333117111f66194a327238ce162eb1e21345f624
Mayoor <mayoordoiphode@gmail.com>
2022-04-12 00:16:37 +0530

    Playing till player reaches exact position of 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,30 +3,31 @@ package com.bridgelabz.assignment4.SnakeLadder;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to Snake And ladder Program");	
-		int playerPos = 0;
-		
-		while (playerPos<100) {
-		byte dice = (byte)(Math.random()*6 + 1 );
-		System.out.println("Dice Show: " + dice);
-		byte playerOptions = (byte)(Math.random()*3 );
-		
-		switch (playerOptions) {
-		
-		case 0:
-			System.out.println("Player stays at same postion");
-			break;
-			
-		case 1:
-			System.out.println("Player played & got a Ladder");
-			playerPos = playerPos + dice;
-			break;
-			
-		default:
-			System.out.println("Player played & got a Snake");
-			playerPos = playerPos - dice;
-			if (playerPos <= 0) {
-				playerPos = 0;}
-			break;}
-		System.out.println("Player is at position : " + playerPos); } }
+
+			System.out.println("welcome to Snake And ladder Program");	
+			int playerPosition = 0; //start position
+	
+			while (playerPosition < 100) {	
+				byte dice = (byte)(Math.random()*6 + 1 );
+				System.out.println("Dice Show: " + dice);
+				byte playerOptions = (byte)(Math.random()*3 );
+	
+				switch (playerOptions) {
+				
+				case 0:
+					System.out.println("Player choose to Not to Play");
+					break;
+					
+				case 1:
+					System.out.println("Player played & got a Ladder");
+					playerPosition = playerPosition + dice;
+					break;
+					
+				default:
+					System.out.println("Player played & got a Snake");
+					playerPosition = playerPosition - dice;
+					if (playerPosition <= 0) {
+						playerPosition = 0;	}
+					break;}
+				System.out.println("Player is at position : " + playerPosition);} }
 }
